28a90e3fde2119c1a140db89d027b2b32cb28381
gbtv <44215915+cngbtv@users.noreply.github.com>
2020-05-19 16:45:15 +0800

    Create radio.php

---
+++ radio.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>中文广播网络收音机-中文视听网-手机版</title>
+<meta name="viewport" content="width=device-width; initial-scale=1.0;">
+<link type="text/css" href="rad.css" rel="stylesheet" />
+<!-- <base target="_blank"> -->
+<script src="/images/js/sys.js"></script>
+</head>
+<BODY oncontextmenu="return false" onselectstart="return false" 
+ondragstart="return false" onbeforecopy="return false" oncopy=document.selection.empty() onselect=document.selection.empty()>
+<center>
+  <div class="clistz">
+    <a href="https://www.gbtv.eu.org"><img src="/images/default/logo.jpg" height=90 width=360 border="0" /></a>
+</div>
+<div class="clistz"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- food300 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-0111681478366415"
+     data-ad-slot="7638877674"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
+
+<div class="clistz">
+<div class="clistb">
+<div class="box">
+<div class="boxtopj">正在播放：广播节目</div></div></div>
+    <div class="" id="pmain"><script language="javascript"> 
+<!--
+function getSearch()
+{
+	try{
+	var url=location.search.replace(/&amp;/g,'#'),i;
+	var lss=url.substr(url.indexOf('?')+1).split('&');
+	for(i=0;i<lss.length;i++) if (lss[i].toLowerCase().indexOf(arguments[0].toLowerCase()+'=')==0) return decodeURIComponent(lss[i].substr(lss[i].indexOf('=')+1).replace(/\#/g,'&amp;'));
+	} catch(e) {}
+	return '';
+}
+//-->
+</script>
+<script type="text/javascript" src="rabaidu.js"></script></div>
+</div>
+<div class="clistz">
+ <div class="clistb">
+<div class="box">
+          <div class="boxtopj" id="mt1"><a href="https://www.gbtv.eu.org">温馨提示：点击此处返回网站首页，观看更多电视节目！</a></div>
+</div>
+<div class="box">
+<div class="boxtopj" id="mt2" onclick="setmenu('2');">中文广播台</div>
+<div class="boxmain" id="m2">
+<a href="radio.php?id=386">中国之声</a>
+<a href="radio.php?id=387">经济之声</a>
+<a href="radio.php?id=388">音乐之声</a>
+<a href="radio.php?id=4995">中文环球</a>
+<a href="radio.php?id=276">第一财经</a>
+<a href="radio.php?id=1744">海峡之声</a>
+<a href="radio.php?id=5039">乡村之声</a>
+<a href="radio.php?id=395">文艺之声</a>
+<a href="radio.php?id=389">经典音乐</a>
+<a href="radio.php?id=1005">环球资讯</a>
+</div>
+</div>
+ </div><!--end clist-->
+</div><!--end clistsc-->
+
+<div class="clistz">中文视听网主要提供国内国外的中文广播节目网络高清电视，为朋友们提供高清电视直播，包括中央电视台、各地卫视直播、港澳台电视直播以及电影频道直播等。欢迎大家收听收看中文视听网广播电视节目！<script type="text/javascript"> 
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F5e76c6c650acd4d265a51b45d1b56abc' type='text/javascript'%3E%3C/script%3E"));
+</script> 温馨提示：中文视听网 中文视听论坛 欢迎大家加入
+</div>
+</center>
+</body>
+</html>
